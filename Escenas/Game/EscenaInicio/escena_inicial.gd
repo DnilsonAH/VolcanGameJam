9a6c2f2cf6e359bff95dbf799b79689f0df9adc5
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-09-21 12:29:45 -0500

    EscenaVideoInicial

---
+++ Escenas/Game/EscenaInicio/escena_inicial.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var videoInicial = $VideoStreamPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	videoInicial.play()
+	video_pause(videoInicial)
+	
+	
+	pass # Replace with function body.
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		videoInicial.stop()
+		get_tree().change_scene_to_file("res://Escenas/Game/Place/escena1_plaza.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+func video_pause(video):
+	await get_tree().create_timer(6.8).timeout
+	video.pause()
