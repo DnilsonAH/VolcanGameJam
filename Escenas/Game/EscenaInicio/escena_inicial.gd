dbeba0fbb06ee44b6c0d422cb040416d27173df7
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-09-21 12:33:27 -0500

    EscenaVideo

--- Escenas/Game/EscenaInicio/escena_inicial.gd
+++ Escenas/Game/EscenaInicio/escena_inicial.gd
@@ -5,20 +5,22 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	videoInicial.play()
-	video_pause(videoInicial)
+	video_pause()
+	
+	
 	
 	
 	pass # Replace with function body.
 	
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		videoInicial.stop()
-		get_tree().change_scene_to_file("res://Escenas/Game/Place/escena1_plaza.tscn")
+#func _process(delta: float) -> void:
+#	if Input.is_action_just_pressed("ui_accept"):
+#		videoInicial.stop()
+#		get_tree().change_scene_to_file("res://Escenas/Game/Place/escena1_plaza.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
 
-func video_pause(video):
-	await get_tree().create_timer(6.8).timeout
-	video.pause()
+func video_pause():
+	await get_tree().create_timer(7.5).timeout
+	get_tree().change_scene_to_file("res://Escenas/Game/Place/escena1_plaza.tscn")
