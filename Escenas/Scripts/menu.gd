debdfd5e7b6d82350dca49d06d7f7e9ff3c79149
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-09-21 03:14:46 -0500

    Modificacion de tranciciones, y musica de menu

--- Escenas/Scripts/menu.gd
+++ Escenas/Scripts/menu.gd
@@ -1,15 +1,31 @@
 extends Control
 
+@onready var musica: AudioStreamPlayer = $AudioStreamPlayer
+
+var musica_de_gamplay = preload("res://Resources/Songs/Musica nostalgia 8 bit.mp3")
+
+#Botones del menu
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas/Game/Place/escena1_plaza.tscn")
+	get_tree().change_scene_to_file("res://Escenas/Transiciones/Inicio/HistoriaInicio.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/Menu/Options.tscn")
 
-
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/Menu/Credits.tscn")
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+	
+#Funcion de musica play
+func _ready() -> void:
+	musica.stream = musica_de_gamplay
+	musica.play()
+	
+
+func _inputPause(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_letter_p"):
+		if !musica.stream_paused:
+			musica.stream_paused = true
+		else:
+			musica.stream_paused = false
---
+++ Escenas/Transiciones/Inicio/historia_inicio.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var isEsceneRutes = [
+	"res://Resources/EscenasTrantitions/HistoriaInicial/escena1.png", 
+	"res://Resources/EscenasTrantitions/HistoriaInicial/escena2.png",
+    "res://Resources/EscenasTrantitions/HistoriaInicial/escena3.png"
+]
+
+@onready var isEscenes = $TextureEscena
+
+func _ready() -> void:
+	await show_image_sequence()
+	
+
+func show_image_sequence():
+	for route in isEsceneRutes:
+		isEscenes.texture = load(route)
+		await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://Escenas/Transiciones/Inicio/Inicia_Game.tscn")
+	
