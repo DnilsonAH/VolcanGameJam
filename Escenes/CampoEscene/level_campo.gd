cab76196f0428fede013cb6a2f738ab052a0309c
Deyson34 <deyson.flores@tecsup.edu.pe>
2025-09-22 04:58:45 -0500

    Al tomar hoja volver biblioteca

---
+++ Escenes/CampoEscene/level_campo.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		var game = get_tree().get_nodes_in_group("GAME")[0]
+		game.siguiente_nivel = "mundo2"
+		game._verificar_nivel()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		var game = get_tree().get_nodes_in_group("GAME")[0]
+		game.siguiente_nivel = "mundo2"
+		game._verificar_nivel()
